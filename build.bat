5962b5afd218326376086c9d4f4f6881525d6b0e
Toni Petrina <tonipetrina@hotmail.com>
2015-08-04 16:20:16 +0200

    Updated NuGet and build files

--- build.bat
+++ build.bat
@@ -14,10 +14,12 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
+nuget restore src
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\YAWL.Common.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
-mkdir Build\lib\net40
 
 %nuget% pack "src\YAWL.Common.Portable\YAWL.Common.Portable.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "src\YAWL.Common.WindowsPhone81\YAWL.Common.WindowsPhone81.csproj" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
