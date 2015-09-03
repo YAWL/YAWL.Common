9a6dbf4427938fb5da99472d149d47b2ca329368
Toni Petrina <tpetrina@tpetrina.com>
2015-09-03 23:15:47 +0200

    Added UWP project, removed WindowsPhone 8.1 project

--- src/YAWL.Common.UWP/Properties/AssemblyInfo.cs
+++ src/YAWL.Common.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("YAWL.Common.WindowsPhone81")]
+[assembly: AssemblyTitle("YAWL.Common.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YAWL.Common.WindowsPhone81")]
+[assembly: AssemblyProduct("YAWL.Common.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
